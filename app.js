bcc7e81bc1c2c73ba644c809d328bad5dbb7c100
Carlos Dev <contacto@carlosdev.site>
2021-03-05 15:56:47 -0600

    second commit

--- app.js
+++ app.js
@@ -1,196 +1,78 @@
-/* 
-  IP Address Tracker/Locator on map.
-  Gets City, Region, Country, Zip, Timezone, ISP
-*/
+const form = document.querySelector("form"),
+  inpt = document.querySelector("form input"),
+  IP_ADDRESS = document.querySelector(".ip_address"),
+  LOCATION = document.querySelector(".location"),
+  TIMEZONE = document.querySelector(".timezone"),
+  ISP = document.querySelector(".isp"),
+  loader = document.querySelector(".loader");
+let lat;
+let long;
+let map;
 
-const input = document.getElementById("ip-input");
-const searchButton = document.getElementById("submit");
-const ipElement = document.getElementById("ip-address");
-const locationElement = document.getElementById("location");
-const timezoneElement = document.getElementById("timezone");
-const offsetElement = document.getElementById("offset");
-const ispElement = document.getElementById("isp");
-const resultsContainer = document.getElementsByClassName("results-container");
-const hideShow = document.getElementById("hide-show");
-let popupMsg = document.createElement("div");
-let mymap;
-let marker;
-const zoomSize = 15;
-const proxyURL = "https://quicors.herokuapp.com/";
+// IP Geolocation API
+const apiUrl =
+  "https://geo.ipify.org/api/v1?apiKey=at_ajEeLRJSALHYCFFsXTln5bE7iQ86X";
 
+fetch("https://api.ipify.org?format=json")
+  .then(res => res.json())
+  .then(data => fetchGeolocation(data.ip));
 
-// Load
-locateIP();
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  const val = inpt.value.trim();
 
-// HTTP Request using plain js
-function locateIP() {
-	let ipAddress = input.value;
-	const xhr = new XMLHttpRequest(),
-		method = "GET",
-		url = proxyURL + `http://ip-api.com/json/${ipAddress}?fields=33580031`;
-
-	xhr.open(method, url, true);
-	xhr.onreadystatechange = function () {
-		// In local files, status is 0 upon success in Mozilla Firefox
-		if (xhr.readyState === XMLHttpRequest.DONE) {
-			const status = xhr.status;
-			if (status === 0 || (status >= 200 && status < 400)) {
-				const data = JSON.parse(xhr.responseText); // Convert json response to js object
-				// console.log("xhr: ", xhr);
-				enterAddressInfo(data);
-				enterMapInfo(data);
-			} else {
-				alert("There was a problem connecting.");
-			}
-		}
-	};
-	xhr.send();
-}
-
-// Fill in results container
-function enterAddressInfo(data) {
-	// Destructure data object
-	const { lat, lon, query, city, region, regionName, country, countryCode, zip, timezone, offset, isp } = data;
-
-	// Address not found or available
-	if (!city) {
-		ipElement.textContent = "Not Found";
-		locationElement.textContent = "-";
-		timezoneElement.textContent = "-";
-		offsetElement.textContent = "";
-		ispElement.textContent = "-";
-	} else {
-		ipElement.textContent = query;
-		locationElement.textContent = `${city}, ${region} ${country} ${zip}`;
-		const utc = msToTime(offset); // Format UTC
-		const sign = offset > 0 ? "+" : "";
-		timezoneElement.textContent = timezone;
-		offsetElement.textContent = `UTC ${sign}${utc}`;
-		ispElement.textContent = isp;
-		popupMsg.innerHTML = `${query}<br>${city}, ${region} ${country} ${zip}<br>UTC: ${sign}${utc}<br>${isp}<br>Latitude: ${lat}<br>Longitude: ${lon}`;
-	}
-}
-
-// Calculate then formats UTC
-function msToTime(s) {
-	// Pad to 2 or 3 digits, default is 2
-	function pad(n, z) {
-		z = z || 2;
-		return ("00" + n).slice(-z);
-	}
-
-	let secs = s % 60;
-	s = (s - secs) / 60;
-	let mins = s % 60;
-	let hrs = (s - mins) / 60;
-
-	return pad(hrs) + ":" + pad(mins);
-}
-
-// Marker icon
-const dropIcon = L.icon({
-	iconUrl: "images/icon-location.svg",
-	iconSize: [30, 40], // size of the icon
-	shadowSize: [50, 64], // size of the shadow
-	iconAnchor: [14, 0], // point of the icon which will correspond to marker's location
-	// iconAnchor: [14, -50], // point of the icon which will correspond to marker's location
-	shadowAnchor: [4, 62], // the same for the shadow
-	popupAnchor: [0, 0] // point from which the popup should open relative to the iconAnchor
-	// popupAnchor: [2, 50] // point from which the popup should open relative to the iconAnchor
+  fetchGeolocation(val);
 });
 
-// Fill in map coordinates
-function enterMapInfo(data) {
-	try {
-		if (mymap) {
-			loadMapView(data); // If map already loaded just render map view
-		} else {
-			initialMapLoad(data); // If map not yet loaded, load.
-		}
+const fetchGeolocation = async ip => {
+  const res = await fetch(`${apiUrl}&ipAddress=${ip}`);
+  const data = await res.json();
 
-		if (marker) {
-			mymap.removeLayer(marker); // remove existing marker before creating new one
-		}
+  lat = data.location.lat;
+  long = data.location.lng;
 
-		marker = new L.marker([data.lat, data.lon], {
-			icon: dropIcon
-		})
-			.addTo(mymap)
-			.bindPopup(popupMsg)
-			.closePopup();
+  const locationInfo = `${data.location.city}, ${data.location.country} ${data.location.postalCode}`;
 
-		if (hideShow.innerHTML === "Details") {
-			marker.openPopup();
-		}
-	} catch (e) {
-		console.log(e);
-	}
-}
+  IP_ADDRESS.textContent = data.ip;
+  ISP.textContent = data.isp;
+  LOCATION.textContent = locationInfo;
+  TIMEZONE.textContent = data.location.timezone;
 
-// Initial loading of mymap and during page refresh/reload
-function initialMapLoad(coord) {
-	mymap = L.map("map-container").setView([coord.lat, coord.lon], zoomSize);
+  setMap(lat, long, locationInfo);
+  inpt.value = "";
+};
 
-	// If using OpenStreetMap tiles
-	L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-		maxZoom: 19,
-		attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-	}).addTo(mymap);
+const setMap = async (lat, long, locationInfo) => {
+  if (map) {
+    map.remove();
+    map = await L.map("map", {
+      center: [lat, long],
+      zoom: 13,
+      preferCanvas: true,
+      zoomControl: false,
+    });
+  } else {
+    map = await L.map("map", {
+      center: [lat, long],
+      zoom: 13,
+      preferCanvas: true,
+      zoomControl: false,
+    });
+  }
 
-	// show the scale bar on the lower left corner
-	L.control.scale().addTo(mymap);
-}
+  const markerIcon = L.icon({
+    iconUrl: "/images/icon-location.svg",
+  });
 
-// Changing map view without reloading or refreshing page
-function loadMapView(coord) {
-	try {
-		mymap.setView([coord.lat, coord.lon], zoomSize);
-	} catch (e) {
-		console.log(e);
-		alert("Please enter a valid ip or domain address to search.");
-	}
-}
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}", {
+    foo: "bar",
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+  }).addTo(map);
 
-function handleClick(e) {
-	e.preventDefault(); // Make changes without reloading page
-	locateIP();
-}
-
-searchButton.addEventListener("click", handleClick);
-
-function hideResultsContainer(e) {
-	e.preventDefault();
-	if (hideShow.innerHTML === "Hide") {
-		resultsContainer[0].style.visibility = "hidden";
-		hideShow.innerHTML = "Details";
-		hideShow.title = "Show details";
-		hideShow.style.right = "0";
-		hideShow.style.visibility = "visible";
-		marker.openPopup();
-	} else {
-		resultsContainer[0].style.visibility = "visible";
-		hideShow.innerHTML = "Hide";
-		hideShow.title = "Hide details";
-		hideShow.style.right = "10px";
-		hideShow.style.visibility = "visible";
-		marker.closePopup();
-	}
-}
-
-hideShow.addEventListener("click", hideResultsContainer);
-
-function moveDetails(x) {
-	if (x.matches && hideShow.innerHTML == "Details") {
-		hideShow.style.right = "0";
-	} else {
-		hideShow.style.right = "10px";
-	}
-}
-const screenW = window.matchMedia("(max-width: 900px)");
-screenW.addEventListener("change", screenW => {
-	if (screenW.matches && hideShow.innerHTML == "Details") {
-		hideShow.style.right = "0";
-	} else {
-		hideShow.style.right = "10px";
-	}
-});
+  const marker = L.marker([lat, long], {
+    icon: markerIcon,
+    title: "You are here",
+  }).addTo(map);
+  marker.bindPopup(locationInfo).openPopup();
+};
